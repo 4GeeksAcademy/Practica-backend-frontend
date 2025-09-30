535b326944c269b642b00f5d5002e38c578d39a6
Incerteza88 <info@incerteza.es>
2025-09-30 11:36:32 +0000

    lo dejo limpio para empezarlo

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -6,17 +6,7 @@ import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
 
 
 export const Navbar = () => {
-	const { store, dispatch } = useGlobalReducer()
-	const navigate = useNavigate();
-
-
-	const token = localStorage.getItem("token");
-
-
-	const handleLogout = () => {
-		localStorage.removeItem("token");
-		navigate("/login");
-	};
+	
 
 	return (
 		<nav className="navbar navbar-light">
@@ -26,41 +16,11 @@ export const Navbar = () => {
 				</Link>
 
 
-				{/* hacemos un ternario para mostrar el boton de cerrar sesion o el de iniciar sesion dependiendo de si estamos logueados o no */}
-				{
-					store.user_profile && token ? <div className="ml-auto">
-						<button className="btn btn-danger" onClick={handleLogout}>
-							Cerrar Sesión
-						</button>
-					</div> :
-						<div className="ml-auto">
-							<button className="btn btn-primary" onClick={() => navigate("/login")}>
-								Iniciar sesión
-							</button>
-						</div>
-				}
+				
 
 			</div>
 		</nav>
 	);
+}
 
-
-
-
-
-
-	return (
-		<nav className="navbar navbar-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/login" className="me-2">
-						<button className="btn btn-primary">Iniciar Sesión</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
-};
+	
--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -10,9 +10,7 @@ import { Home } from "./pages/Home";
 import { Single } from "./pages/Single";
 import { Demo } from "./pages/Demo";
 
-import RegisterContent from "./components/RegisterContent";
-import LoginContent from "./components/LoginContent";
-import ProfileContent from "./components/ProfileContent";
+
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
@@ -30,9 +28,7 @@ export const router = createBrowserRouter(
         <Route path="/single/:theId" element={ <Single />} />  {/* Dynamic route for single items */}
         <Route path="/demo" element={<Demo />} />
 
-        <Route path="/register" element={<RegisterContent />} />
-        <Route path="/login" element={<LoginContent />} />
-        <Route path="/profile" element={<ProfileContent />} />
+
 
 
       </Route>
